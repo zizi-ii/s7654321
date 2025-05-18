535adbab0cde9afdd550e126692dd48f477eea43
Lee, Jiwoo <zizii23027@gmail.com>
2025-05-18 16:09:16 +0900

    two

--- src/s7654321/Main.java
+++ src/s7654321/Main.java
@@ -3,5 +3,7 @@ package s7654321;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("1234567 이지우");
+		System.out.println(Hello.sum(10)); 
+		System.out.println(Hello.sum(100));
 	}
 }
